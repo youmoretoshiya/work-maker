51c5f2155fb88bac4173405af27b3f8a5445fe38
toshiya miyazaki <toshiyamiyazaki@gmail.com>
2020-04-23 21:56:45 +0900

    20200420 commit

--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "app/index.html")
+
+def home(request):
+    return render(request, "app/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("app:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'app/signup.html',context)
+
+@login_required
+def home(request):
+    return render(request, "app/home.html")
+        
--- project/project/settings.py
+++ project/project/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "app:home"
+LOGOUT_REDIRECT_URL = "app:index"
+LOGIN_URL = "login"
